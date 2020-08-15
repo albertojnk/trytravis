a2816b5ebdb91ed2f589e1971c01c9ec80ea771d
albertojnk <alberto.janicke@redfox.tech>
2020-08-15 01:31:47 -0300

    create table to show preview

---
+++ frontend/src/app/procedure.ts
@@ -0,0 +1,12 @@
+export interface Procedure {
+    consultado: string;
+    extraido: string;
+    comarca: string;
+    o_julgador: string;
+    procedimento: string;
+    ativa: string;
+    passiva: string;
+    e_ativa: string;
+    e_passiva: string;
+    created: string;
+}
